8a695281d69061e94e9d9be85d8a92dc364895f3
antonbrams <dev@antonbrams.io>
2020-06-17 22:50:45 +0200

    link preload change to image

--- src/page/helpers.js
+++ src/page/helpers.js
@@ -34,9 +34,11 @@ const Lerp = (fn, speed = 0.05, threshold = 1) => {
 }
 
 const preload = url => {
-	const link = document.createElement('link')
-	link.href = url
-	link.rel = 'preload'
-	link.as = 'image'
-	document.head.appendChild(link)
+	const img = new Image()
+	img.src = url
+	// const link = document.createElement('link')
+	// link.href = url
+	// link.rel = 'preload'
+	// link.as = 'image'
+	// document.head.appendChild(link)
 }
